e0ee8d75437ffed86903b21361fe1797a2782672
ianthehamster <ianchow9898@gmail.com>
2024-04-06 22:36:50 +0800

    Fixed get requests for categories

--- src/components/productLists/PhonesProductPage.jsx
+++ src/components/productLists/PhonesProductPage.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import ProductCard from "../ProductCard";
-import { Grid } from "@mui/material";
-import Navbar from "../Navbar";
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductCard from '../ProductCard';
+import { Grid } from '@mui/material';
+import Navbar from '../Navbar';
+import { useLocation } from 'react-router-dom';
 
-import { BACKEND_URL } from "../../constantVariables";
-import ModelFlagshipLaptop from "../ModelFlagshipProduct";
+import { BACKEND_URL } from '../../constantVariables';
+import ModelFlagshipLaptop from '../ModelFlagshipProduct';
 const PhonesProductPage = () => {
   const [phones, setPhones] = useState([]);
-  const [modelState, setModelState] = useState("");
+  const [modelState, setModelState] = useState('');
   const location = useLocation();
   const currentUrl = location.pathname; // returns /phonesPage - current path
-  const categoryName = currentUrl.replace(/Page$/, "").substring(1); //returns phones as category name
+  const categoryName = currentUrl.replace(/Page$/, '').substring(1); //returns phones as category name
   const params = { categoryName: categoryName };
 
+  // useEffect(() => {
+  //   axios.get(`${BACKEND_URL}/products`, { params }).then((response) => {
+  //     setPhones(response.data);
+  //     setModelState("phones");
+  //   });
+  // }, []);
+
   useEffect(() => {
-    axios.get(`${BACKEND_URL}/products`, { params }).then((response) => {
+    axios.get(`${BACKEND_URL}/products?${categoryName}`).then((response) => {
       setPhones(response.data);
-      setModelState("phones");
+      setModelState('phones');
     });
   }, []);
 
