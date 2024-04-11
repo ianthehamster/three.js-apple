33beee11ebee9062f7026f15e2fcecf32d5dba0b
Mannushka <zaimar1910@gmail.com>
2024-04-12 01:32:49 +0800

    edit orders page, make it responsive

--- src/components/orders/OrderCard.jsx
+++ src/components/orders/OrderCard.jsx
@@ -12,26 +12,30 @@ import {
 } from "@mui/material";
 import { formatCurrency } from "../../utils/formatCurrency";
 import "./Orders.css";
+import { useNavigate } from "react-router-dom";
 
 const OrderCard = (props) => {
+  const navigate = useNavigate();
   const orderPrice = formatCurrency(props.order.total_price);
-
   const formattedDate = new Date(props.order.createdAt).toLocaleString();
-  const address = props.order.address.address;
+  // const address = props.order.address.address;
 
   const products = props.order.products;
 
   const orderedProducts = products.map((product) => (
     <div key={product.id} className="order-products">
-      <Stack spacing={2}>
-        <div className="product-info">
-          <div>{product.title}</div>
-          <div className="product-in-order-img">
-            <img src={product.img && product.img} alt="product_img" />
-          </div>
-          <div>{`x${product.order_products.quantity}`}</div>
+      {/* <Stack spacing={2}> */}
+      <div className="product-info">
+        <div className="product-title">{product.title}</div>
+        <div className="product-in-order-img">
+          <img src={product.img && product.img} alt="product_img" />
         </div>
-      </Stack>
+        <div className="quantity">x{product.order_products.quantity}</div>
+        <div className="price">
+          {product.price && formatCurrency(product.price)}
+        </div>
+      </div>
+      {/* </Stack> */}
     </div>
   ));
   const orderCard = (
@@ -46,22 +50,32 @@ const OrderCard = (props) => {
             <Typography variant="body2" color="text.secondary">
               Created: {formattedDate}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Total: {orderPrice}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
+
+            {/* <Typography variant="body2" color="text.secondary">
               Shipped to: {address && address}
-            </Typography>
+            </Typography>  */}
           </div>
           <Divider />
           {orderedProducts}
+          <div className="total-price">
+            <Typography gutterBottom variant="h6" component="div">
+              Total: {orderPrice}
+            </Typography>
+          </div>
         </CardContent>
 
-        <CardActions className="order-card-actions">
+        {/* <CardActions className="order-card-actions">
           <div className="order-card-btn">
-            <Button size="small">View details</Button>
+            <Button
+              variant="outlined"
+              size="small"
+              sx={{ color: "black" }}
+              onClick={() => navigate(`/my-orders/${props.order.id}`)}
+            >
+              View details
+            </Button>
           </div>
-        </CardActions>
+        </CardActions> */}
       </Card>
     </div>
   );
