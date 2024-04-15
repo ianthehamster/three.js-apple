dc45412f1d5862c15d456a50e56553b779c90365
ianthehamster <ianchow9898@gmail.com>
2024-04-15 22:15:23 +0800

    Deleted share Button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { formatCurrency } from "../utils/formatCurrency";
-import { useNavigate } from "react-router-dom";
-import { useTheme } from "@mui/material/styles";
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { formatCurrency } from '../utils/formatCurrency';
+import { useNavigate } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 
 const ProductCard = ({ product }) => {
   const price = formatCurrency(product.price);
@@ -25,13 +25,13 @@ const ProductCard = ({ product }) => {
           width: 370,
           height: 400,
           boxShadow:
-            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19) ",
+            '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19) ',
         }}
       >
         <CardContent>
           <CardMedia
             component="img"
-            sx={{ height: 250, objectFit: "contain" }} //
+            sx={{ height: 250, objectFit: 'contain' }} //
             image={product.img}
             alt="product"
           />
@@ -43,10 +43,7 @@ const ProductCard = ({ product }) => {
             {price}
           </Typography>
         </CardContent>
-        <CardActions sx={{ display: "flex", justifyContent: "space-evenly" }}>
-          <div>
-            <Button size="small">Share</Button>
-          </div>
+        <CardActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
           <div>
             <Button size="small" onClick={handleLearnClick}>
               Learn More
