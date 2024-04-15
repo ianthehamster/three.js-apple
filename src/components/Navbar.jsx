0dedcaf0d949ca55590684dd7a411bd9577fbfd8
Mannushka <zaimar1910@gmail.com>
2024-04-15 21:16:30 +0800

    add my orders btn to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import { appleImg, bagImg, searchImg } from '../utils';
+import { appleImg, bagImg, searchImg } from "../utils";
 // import { navLists } from "../constants";
-import { Link } from 'react-router-dom';
-import DevicesIcon from '@mui/icons-material/Devices';
-import BasicMenu from './minorComponents/DropDownMenu';
-import { Button, Typography, Badge } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './buttons/LoginButton';
-import LogoutButton from './buttons/LogoutButton';
-import { CartContext } from '../context/CartContext';
-import { useContext, useEffect } from 'react';
-import axios from 'axios';
-import { BACKEND_URL } from '../constantVariables';
-import { mainLogo } from '../utils';
+import { Link } from "react-router-dom";
+import DevicesIcon from "@mui/icons-material/Devices";
+import BasicMenu from "./minorComponents/DropDownMenu";
+import { Button, Typography, Badge } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./buttons/LoginButton";
+import LogoutButton from "./buttons/LogoutButton";
+import { CartContext } from "../context/CartContext";
+import { useContext, useEffect } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../constantVariables";
+import { mainLogo } from "../utils";
 
-import { NavMobile } from './NavMobile';
+import { NavMobile } from "./NavMobile";
 
 const Navbar = ({ isUserInDb }) => {
   const navigate = useNavigate();
@@ -39,9 +39,9 @@ const Navbar = ({ isUserInDb }) => {
   };
 
   useEffect(() => {
-    console.log('useEffect is called but postNewUser is not called');
+    console.log("useEffect is called but postNewUser is not called");
     if (user && isUserInDb === false) {
-      console.log('useEffect is called correctly');
+      console.log("useEffect is called correctly");
       postNewUser();
     }
   }, [user, isUserInDb]);
@@ -49,26 +49,26 @@ const Navbar = ({ isUserInDb }) => {
   return (
     <header
       className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-black"
-      style={{ width: '100%' }}
+      style={{ width: "100%" }}
     >
       <nav className="flex w-full screen-max-width">
         <img
           src={mainLogo}
           alt="App Logo"
-          style={{ width: '120px', height: 'auto' }}
+          style={{ width: "120px", height: "auto" }}
         />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           <div>
             <Button
               id="basic-button"
-              aria-controls={open ? 'basic-menu' : undefined}
+              aria-controls={open ? "basic-menu" : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
+              aria-expanded={open ? "true" : undefined}
               onClick={() => {
-                navigate('/');
+                navigate("/");
               }}
-              style={{ color: 'white' }}
+              style={{ color: "white" }}
             >
               Home
             </Button>
@@ -80,13 +80,13 @@ const Navbar = ({ isUserInDb }) => {
           <div>
             <Button
               id="basic-button"
-              aria-controls={open ? 'basic-menu' : undefined}
+              aria-controls={open ? "basic-menu" : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
+              aria-expanded={open ? "true" : undefined}
               onClick={() => {
-                navigate('/aboutUs');
+                navigate("/aboutUs");
               }}
-              style={{ color: 'white' }}
+              style={{ color: "white" }}
             >
               About us
             </Button>
@@ -101,20 +101,36 @@ const Navbar = ({ isUserInDb }) => {
               width={18}
               height={18}
               onClick={() => {
-                navigate('/cart');
+                navigate("/cart");
               }}
             />
           </Badge>
           <div
             style={{
-              color: 'white',
-              ...(isSmallScreen && { marginRight: '30px' }),
+              color: "white",
+              ...(isSmallScreen && { marginRight: "30px" }),
             }}
           >
             Hello {isAuthenticated ? `${user.first_name}` : `Guest`}
           </div>
+          <div className="max-sm:hidden">
+            {isAuthenticated && (
+              <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={() => {
+                  navigate("/my-orders");
+                }}
+                style={{ color: "white" }}
+              >
+                My orders
+              </Button>
+            )}
+          </div>
 
-          <div className="hidden md:block" style={{ color: 'white' }}>
+          <div className="hidden md:block" style={{ color: "white" }}>
             {isAuthenticated ? <LogoutButton /> : <LoginButton />}
           </div>
         </div>
